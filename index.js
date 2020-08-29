abdcbfc04ad5658c59c25ef89444a85d2328d9d0
mister-blanket <michael@mrplunkett.com>
2020-08-29 14:48:39 -0500

    Invert gamma

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 window.ondeviceorientation = e => handleMove(e);
 
 function handleMove(event) {
-  event.gamma
+
+  let z = Math.floor(event.alpha);
+  let y = Math.floor(event.beta);
+  let x = Math.floor(event.gamma);
+
+  x = y > 90 || y < -90 ? - x : x;
 
   const orientationInfo = `
-    <p>alpha (z): ${Math.floor(event.alpha)}</p>
-    <p>beta (y): ${Math.floor(event.beta)}</p>
-    <p>gamma (x): ${Math.floor(event.gamma)}</p>
+    <p>alpha (z): ${z}</p>
+    <p>beta (y): ${y}</p>
+    <p>gamma (x): ${x}</p>
   `
 
   document.querySelector(".orientation-container").innerHTML = orientationInfo;
